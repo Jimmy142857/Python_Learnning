c20b581dca29f91d23a22507a52afb90b0f70455
Jimmy <1406218553@qq.com>
2022-02-25 17:55:18 +0800

    updated

--- list_basic.py
+++ list_basic.py
@@ -10,8 +10,8 @@
 # print('\n')
 # names=['Jimmy','Joy','Zf']
 # print(f"Nice to meet u,{names[0]}!\n"
-#      f"Nice to meet u,{names[1]}!\n"
-#      f"Nice to meet u,{names[-1]}!")
+#       f"Nice to meet u,{names[1]}!\n"
+#       f"Nice to meet u,{names[-1]}!")
 
 
 # motorcycles=['honda','yamaha','suzuki']
@@ -21,7 +21,7 @@
 # motorcycles.append('honda')                                  # append(元素):给列表末尾附加一个元素
 # print(motorcycles)
 # print('\n')
-# motorcycles=[]
+# motorcycles=[]                                               # 初始化空列表
 # motorcycles.append('honda')                                  # append()动态的创建列表
 # motorcycles.append('yamaha')
 # motorcycles.append('suzuki')
@@ -40,16 +40,16 @@
 
 # motorcycles=['honda','yamaha','suzuki']
 # print(motorcycles)
-# popped_motorcycles=motorcycles.pop()                                # pop():默认删除列表末尾的元素，并让你能够接着使用它（弹出栈顶元素）  
+# popped_motorcycles=motorcycles.pop()                                 # pop():默认删除列表末尾的元素，并让你能够接着使用它（弹出栈顶元素）  
 # print(motorcycles)
 # print(popped_motorcycles)
 # print('\n')
 # motorcycles=['honda','yamaha','suzuki']
 # last_owned=motorcycles.pop()
-# print(f"The last motorcycle I owned is a {last_owned.title()}")     # example
+# print(f"The last motorcycle I owned is a {last_owned.title()}.")     # example
 # motorcycles=['honda','yamaha','suzuki']
-# first_owned=motorcycles.pop(0)                                      # pop(0):弹出列表的第一个元素，括号可指定任意位置
-# print(f"The first motorcycle I owned is a {first_owned.title()}")
+# first_owned=motorcycles.pop(0)                                       # pop(0):弹出列表的第一个元素，括号可指定任意位置
+# print(f"The first motorcycle I owned is a {first_owned.title()}.")
 
 
 # motorcycles=['honda','yamaha','suzuki','ducati']
@@ -62,7 +62,7 @@
 # too_expensive='ducati'                                              # 接着使用删除元素的值
 # motorcycles.remove(too_expensive)
 # print(motorcycles)
-# print(f"\nA {too_expensive.title()} is too expensive for me.")
+# print(f"\nA {too_expensive.title()} is too expensive for me.\n")
 #####################################################################
 
 
@@ -74,8 +74,9 @@ print(names)
 new_guy="tom"
 poor_guy=names.pop()
 names.append(new_guy)
-print(f"Sorry,{poor_guy.title()} can't come for dinner. "
-      f"We invite {new_guy.title()} instead:")
+print(f"Sorry, {poor_guy.title()} can't come for dinner. "
+      f"We invite {new_guy.title()} instead."
+      f"\nThe members now are:")
 print(names)
 print('\n')
 
@@ -94,6 +95,7 @@ poor4=names.pop()
 print(f"I am sorry, {poor1}, {poor2}, {poor3}, and {poor4}.")
 print(f"{names[0].title()} and {names[1].title()}, u two are the left.")
 
+print('\n')
 del names[0]
 del names[0]
 print(names)
@@ -103,8 +105,11 @@ print(names)
 
 #######################################################
 # cars=['bmw','audi','toyota','subaru']
+# print(f"{cars}\n")
+# print(cars,'\n')  
 # cars.sort()                                            # sort():从小到大，永久性的修改列表元素的排列顺序
-# print(cars) 
+# print(cars)
+# print()                                                # print()：函数默认有一个换行参数 end='\n',可实现换行
 # cars.sort(reverse=True)                                # reverse=True:传递参数，使函数功能反向
 # print(cars)
 
@@ -119,7 +124,7 @@ print(names)
 # cars=['bmw','audi','toyota','subaru']                  # reverse():永久性的反转列表元素的排列顺序
 # print(cars)
 # cars.reverse()
-# print(cars)
+# print('\n',cars,sep='')                                # print()：函数默认有一个空格参数 end='\n'
 
 # a=len(cars)                                            # len():快速获悉列表的长度
 # print(a)
